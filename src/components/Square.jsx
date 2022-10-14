8d5d77791a0591aebbdfeeaeca0e9a8321a139d3
Harsh Kumar <harshkumarmnb@gmail.com>
2022-10-14 11:53:04 +0530

    added board state and also react events

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Square.style.css';
 
-const Square = ({ value }) => {
+const Square = ({ value, onClick }) => {
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
