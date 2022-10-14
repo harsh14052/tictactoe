5d06e8a54ff3cc0685cadee502388f69c1bb143f
Harsh Kumar <harshkumarmnb@gmail.com>
2022-10-14 15:54:45 +0530

    game complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './components/Board';
 import './styles/root.scss';
+import { calculateWinner } from './helpers.js';
 
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isNext, setIsNext] = useState(false);
+
+  const winner = calculateWinner(board);
+  console.log(winner);
+  const handleSquarelick = position => {
+    if (board[position]) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isNext ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+
+    setIsNext(prev => !prev);
+  };
+
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <Board />
+      <h2>message</h2>
+      <Board board={board} handleSquarelick={handleSquarelick} />
     </div>
   );
 };
